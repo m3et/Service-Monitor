cb31e267c2ba4cabfa15747d2aae8f826c75a253
Raphael Gozlan <raphaelior09@gmail.com>
2020-05-03 14:18:29 +0300

    Pre .gitignore changes

--- logs.py
+++ logs.py
@@ -1,21 +1,45 @@
+import os
+from os import path
 import datetime
-
 
 serviceList = 'serviceList.txt'
 statusLog = 'statusLog.txt'
 
 
-def print_to_file(file_name, input):
-    with open(file_name, 'a+') as write_obj:
-        write_obj.write(input + '\n')
+def print_to_log(file_name, line):
+    if not path.exists(file_name):
+        append_print_to_log(file_name, line)
+
+    """ Insert given string as a new line at the beginning of a file """
+    # define name of temporary dummy file
+    dummy_file = file_name + '.bak'
+    # open original file in read mode and dummy file in write mode
+    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        # Write given line to the dummy file
+        write_obj.write(line + '\n')
+        # Read lines from original file one by one and append them to the dummy file
+        for line in read_obj:
+            write_obj.write(line)
+    # remove original file
+    os.remove(file_name)
+    # Rename dummy file as the original file
+    os.rename(dummy_file, file_name)
+
+    read_obj.close()
     write_obj.close()
 
 
-def add_timestamp(input):
+def append_print_to_log(file_name, line):
+    with open(file_name, 'a') as write_obj:
+        write_obj.write(line + '\n')
+    write_obj.close()
+
+
+def add_timestamp(line):
     timestamp = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' + datetime.datetime.now().strftime(
         "%Y-%m-%d %H:%M:%S") + '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-    input = timestamp + '\n' + input
-    return input
+    line = timestamp + '\n' + line
+    return line
 
 
 if __name__ == "__main__":
@@ -24,4 +48,4 @@ if __name__ == "__main__":
     for i in range(3):
         proc = services.list_of_process()
         proc = add_timestamp(proc)
-        print_to_file(serviceList, proc)
+        print_to_log(serviceList, proc)
--- monitor_mode.py
+++ monitor_mode.py
@@ -8,14 +8,14 @@ statusLog = 'statusLog.txt'
 def updates_log():
     proc = services.list_of_process()
     proc = logs.add_timestamp(proc)
-    logs.print_to_file(serviceList, proc)
-    # print('Updates Logs\n' + proc)
+    logs.print_to_log(serviceList, proc)
+    print('Updates Logs\n' + proc)
+
+
+def ask_for_time():
+    return float(input("Enter desire second between samples"))
 
 
 if __name__ == "__main__":
     for i in range(3):
         updates_log()
-
-
-def ask_for_time():
-    return float(input("Enter desire second between samples"))
