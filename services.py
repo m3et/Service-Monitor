69b01aa0a123a1ca023eb5a3a1ee4f389e0f9203
Raphael Gozlan <raphaelior09@gmail.com>
2020-05-11 20:43:50 +0300

    services

--- services.py
+++ services.py
@@ -1,11 +1,29 @@
+import subprocess
+
 import psutil
+from platform import system
 
 
 # This function retrieve all running processes in sys in
-def get_dict_of_process():
-    process_dict = {p.pid: p.info for p in psutil.process_iter(['name'])}
-    process_dict = {key: process_dict.get(key).get('name') for key in process_dict}
+def services_linux():
+    output = subprocess.check_output(["service", "--status-all"])
+    print(output)
+
+
+def services_win():
+    process_dict = [s for s in psutil.win_service_iter()]
+    process_dict = {s.pid(): s.name() for s in process_dict}
     return process_dict
+
+
+def get_dict_of_process():
+    os = system()
+    if os == 'Linux':
+        return services_linux()
+    elif os == 'Windows':
+        return services_win()
+    else:
+        raise ValueError('OS id not supported')
 
 
 # This function print all process in system
@@ -15,4 +33,4 @@ def print_dict_of_process():
 
 
 if __name__ == "__main__":
-    print_dict_of_process()
+    services_linux()
